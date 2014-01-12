9a6b0e52328ab28bf3bc42e99be2cd3a1bd4b73f
Nick <nickgobee0122@gmail.com>
2014-01-12 15:30:22 +0100

    SQL_Queries

--- applicatie.rb
+++ applicatie.rb
@@ -4,6 +4,7 @@
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
+require 'geocoder'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'nokogiri'
@@ -12,6 +13,8 @@ require 'dm-migrations'
 require 'dm-timestamps'
 require 'dm-validations'
 require 'dm-constraints'
+
+include Geocoder::Calculations
 
 # RELOAD
 also_reload "#{ settings.root }/applicatie.rb"
@@ -29,11 +32,15 @@ class Vacature
 	property :omschrijving,		String
 	property :version,						Integer
 	
-	has n,	:vacatures_educations, :through => Resource
-	has n, 	:vacatures_locations, :through => Resource
-	has n, 	:vacatures_skills, :through => Resource
-
-	end
+	has n, :vacatures_locations
+	has n, :locations, :through => :vacatures_locations
+	
+	has n, :vacatures_educations
+	has n, :educations, :through => :vacatures_educations
+	
+	has n, :vacatures_skills
+	has n, :skills, :through => :vacatures_skills
+end
 
 class VacaturesEducation
 
@@ -45,8 +52,8 @@ class VacaturesEducation
 	property :vacature_id, 		Integer
 	property :education_id, 	Integer
 	
-	has n,	:vacature, :through => Resource
-	has n, 	:educations, :through => Resource
+	belongs_to :vacature
+	belongs_to :education
 	
 end
 	
@@ -54,11 +61,12 @@ class Education
 
 	include DataMapper::Resource
 	
-	property :id,									Serial
+	property :id,					Serial
 	property :education,			String
-	property :level,							Integer
+	property :level,				Integer
 	
-	has n, 	:vacatures_educations, :through => Resource
+	has n, :vacatures_educations
+	has n, :vacatures, :through => :vacatures_educations	
 	
 end
 	
@@ -68,11 +76,12 @@ class VacaturesLocation
 	
 	storage_names[:default] = "vacatures_locations"
 
-	property :id,									Serial
+	property :id,				Serial
 	property :vacature_id, 		Integer
-	property :location_id,		 	Integer
+	property :location_id,		Integer
 	
-	has n, :vacatures, :through => Resource
+	belongs_to :vacature
+	belongs_to :location
 	
 end
 
@@ -80,13 +89,78 @@ class Location
 
 	include DataMapper::Resource
 	
-	property :id,									Serial
+	property :id,						Serial
 	property :name,						String
 	property :longitude,				Float
 	property :latitude,					Float
-	
-	has n,	:vacatures_locations, :through => Resource
 
+	def self.calc(s1,s2)
+
+		res1		= self.check(s1)
+		res2		= self.check(s2)	
+
+		
+        lat1        = res1.latitude
+        lon1        = res1.longitude
+        
+        lat2        = res2.latitude
+        lon2        = res2.longitude
+               
+        r           = 6371
+        dLat        = (lat2 - lat1) * Math::PI / 180
+        dLon        = (lon2 - lon1) * Math::PI / 180
+        lat1        = res1[:latitude] * Math::PI / 180
+        lat2        = res2[:latitude] * Math::PI / 180
+                
+        a           = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
+        c           = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        d           = r * c
+	end
+
+	def self.check(s1)
+
+		res = first(:name => s1)
+
+		if res.nil?
+
+			c 		= Geocoder.search(s1)
+			long 	= c[0].longitude
+			lat 	= c[0].latitude
+			Location.create(:name => s1, :longitude => long, :latitude => lat) 
+
+		end
+
+		res = first(:name => s1)
+
+	end
+
+	def self.grade(s1, s2, i)
+
+		res 	= 0
+		d 		= self.calc(s1,s2)
+		d 		= d.to_i
+		step 	= i*0.1
+		dif 	= d - i
+		dif2	= (dif / step) * 5
+
+		if d < i
+			res = 100
+		else
+			res = 100 - dif2
+			if res < 0
+				res = 0
+			end
+		end
+
+
+		res
+
+
+	end
+
+	has n, :vacatures_locations
+	has n, :vacatures, :through => :vacatures_locations
+	
 end
 
 class VacaturesSkill
@@ -95,28 +169,39 @@ class VacaturesSkill
 	
 	storage_names[:default] = "vacatures_skills"
 
-	property :id,									Serial
+	property :id,				Serial
 	property :vacature_id, 		Integer
-	property :skill_id,					 	Integer
+	property :skill_id,			Integer	
 	
-	has n, :skills, :through => Resource
+	belongs_to :vacature
+	belongs_to :skill
 	
 end
 
-class Skill
 	
+class Skill
 	include DataMapper::Resource
 	
-	property :id,								Serial
-	property :skill,						String
+	property :id,					Serial
+	property :skill,				String
 
-	has n, 	:vacatures_skills, :through => Resource
+	def self.calc(s1)
+
+		s1 = "%" + s1 + "%"
+
+		res = all(:skill.like => s1)
+
+	end
+	
+	has n, :vacatures_skills
+	has n, :vacatures, :through => :vacatures_skills
+	
 end
 
 configure :development do
 
   #DataMapper.setup( :default, "sqlite3://#{ Dir.pwd }/database.sqlite3")
-  DataMapper.setup( :default, 'mysql://jobhearted:RAM2675132@mysql.insidion.com/jobhearted')
+  DataMapper.setup( :default, 'mysql://jobhearted:RAM2675132@mysql.insidion.com/jobhearted2')
 
 end
 
@@ -129,118 +214,40 @@ get '/' do
   erb :index
 end
 
-get '/string' do
-  @html_doc = Nokogiri::HTML( '<html><body><h1>Mr. Belvedere Fan Club</h1></body></html>' )
-  erb :string, :layout => false
-end
-
-get '/bestand' do
-  bestand   = File.open( "#{ settings.root }/bestanden/demo.html" )
-  @html_doc = Nokogiri::HTML( bestand )
-  bestand.close
-  erb :string, :layout => false
-end
-
-get '/acteur' do
-  bestand  = File.open( "#{ settings.root }/bestanden/shows.xml" )
-  xml_doc = Nokogiri::XML( bestand )
-  @acteurs = xml_doc.xpath( '//character' )
-  bestand.close
-  erb :acteur
-end
-
-get '/serie' do
-  xml_doc = Nokogiri::XML( File.open( "#{ settings.root }/bestanden/shows.xml" ) )
-  @series = xml_doc.xpath( '//name')
-  erb :serie
-end
-
-get '/drama' do
-  # css selector
-  xml_doc = Nokogiri::XML( File.open( "#{ settings.root }/bestanden/shows.xml" ) )
-  @drama  = xml_doc.css( 'dramas name' ).first
-  erb :drama
-end
-
-get '/functies' do
-  html_doc   = Nokogiri::HTML( File.open( "#{ settings.root }/bestanden/starapple.html" ) )
-  @functies = html_doc.xpath( '//functie' )
-  erb :functie
-end
-
-get '/vacatures' do
-  html_doc   = Nokogiri::HTML( File.open( "#{ settings.root }/bestanden/starapple.html" ) )
-  @vacatures = html_doc.css( '#omschrijving' )
-
-  @vacatures.each do |vacature|
-
-    jobs = Vacature.new
-    jobs.functie  = vacature.elements[0].content
-    jobs.locatie  = vacature.elements[1].content
-    jobs.uren     = vacature.elements[2].content
-    jobs.save
-
-  end
-
-  if params['reeks']
-    @reeks = params['reeks']
-  else
-    @reeks = ["Test"]
-  end
-
-  erb :vacature
-end
-
-get '/argumenten' do
-
-  "#{params.inspect}"
-
-end
-
-get '/form' do
-
-  erb :form
-end
-
-post '/verwerk' do
-
-  #vac = Vacature.new
-
-  #vac.functie   = params[:functie]
-  #vac.locatie   = params[:locatie]
-  #vac.uren      = params[:uren]
-
-  #vac.save
-
-  #reeks = [vac.uren]
-
-  redirect "/try?u=#{params[:uren]};c=0"
-
-end
-
-post '/locatie' do
-
-  redirect "/try?u=#{params[:locatie]};c=1"
-
-end
-
-post '/functie' do
-
-  redirect "/try?u=#{params[:functie]};c=2"
-
-end
-
-post '/match' do
-
-  redirect "/try?u=#{params[:locatie]};a=#{params[:uren]};b=#{params[:functie]};c=3"
-
-end
-
-
 get '/try' do
 
-	@q = Vacature.all
+	@s1 = "Rotterdam"
+	@s2 = "Eindhoven"
+	@s3 = "C#"
+	@i 	= 40
+
+	@res 	= Location.grade(@s1, @s2, @i)
+	@res2 	= Location.calc(@s1, @s2)
+	@res3 	= Skill.calc(@s3) 
+
+
+	test = VacaturesEducation.all(:vacature => 
+	[
+		{
+			:bedrijf => "Adecco"
+		}
+	]
+	)
 	
+	a = VacaturesEducation.all(:education_id => 2)
+	@z = a.all(:vacature =>
+	[	
+		{
+			:vacatures_locations =>
+				[
+					{
+						:location_id => 27
+					}
+				]	
+		}
+	]
+)	
+
   erb :try
 
 end
