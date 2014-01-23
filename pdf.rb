1a037e456bcccafa1d7c0b1bd709faa0fdde144e
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2014-01-23 21:27:08 +0100

    Two forms one cup

--- pdf.rb
+++
@@ -1,9 +0,0 @@
-require 'pdf/reader'
-
-filename = File.expand_path(File.dirname(__FILE__)) + "/nise.pdf"
-
-PDF::Reader.open(filename) do |reader|
-  reader.pages.each do |page|
-    puts page.text
-  end
-end
